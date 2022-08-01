c0303ab7fba7b09546cbc66ea6e1b0523d9f40f2
정예원 <yewonn.1j@gmail.com>
2022-08-01 17:33:39 +0900

    feat : logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { User } from './users/user.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
+import { Module, MiddlewareConsumer, NestModule } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { UsersModule } from "./users/users.module";
+import { User } from "./users/user.entity";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { DataSource } from "typeorm";
+import { LoggerMiddleware } from "./common/middlewares/logger.middleware";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
+      type: "mysql",
+      host: "localhost",
       port: +process.env.MYSQL_PORT,
       username: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
@@ -23,8 +24,11 @@ import { DataSource } from 'typeorm';
     UsersModule,
   ],
 })
-export class AppModule {
+export class AppModule implements NestModule {
   constructor(private dataSource: DataSource) {}
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes("*");
+  }
 }
 /*
 
