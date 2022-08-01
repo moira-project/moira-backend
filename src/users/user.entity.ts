f72e515d8ecadccb417325063d84694bcedf9451
정예원 <yewonn.1j@gmail.com>
2022-08-01 19:09:29 +0900

    feat : user entitly validate 추가

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,16 +1,22 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
-
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { IsEmail, IsNotEmpty, IsString, validate } from "class-validator";
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 
   @Column()
+  @IsString()
+  @IsNotEmpty()
   name: string;
 
   @Column({ default: true })
+  @IsString()
   phoneNumber: string;
 
-  @Column({ default: 'user' })
-  role: 'user' | 'admin';
+  @Column({ default: "user" })
+  @IsNotEmpty()
+  role: "user" | "admin";
 }
