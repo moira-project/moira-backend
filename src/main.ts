9c1eb0b14c5237df2abd8cfa403c88df496fab70
정예원 <yewonn.1j@gmail.com>
2022-08-02 15:33:19 +0900

    chore : use global pipe

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { HttpExceptionFilter } from "./common/exceptions/http-exception.filter";
@@ -6,6 +7,7 @@ import { HttpExceptionFilter } from "./common/exceptions/http-exception.filter";
 async function bootstrap() {
   const PORT = process.env.PORT;
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(PORT);
 }
